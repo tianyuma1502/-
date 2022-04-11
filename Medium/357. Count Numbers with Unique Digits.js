172734cc45f1e99a74f8d77d0d9306b3e81a60d9
Tianyu <tianyuma1508977@gmail.com>
2022-04-11 22:21:44 +1000

    initial commit

---
+++ Medium/357. Count Numbers with Unique Digits.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ * Example 1:
+ *
+ * Input: n = 2
+ * Output: 91
+ * Explanation: The answer should be the total numbers in the range of 0 ≤ x < 100, excluding 11,22,33,44,55,66,77,88,99
+ *
+ * Example 2:
+ *
+ * Input: n = 0
+ * Output: 1
+ */
+var countNumbersWithUniqueDigits = function(n) {
+
+};
