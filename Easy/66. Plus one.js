9a14ad3b4d732a24649db6416d3c851b49cd6795
Tianyu <tianyuma1508977@gmail.com>
2022-04-14 01:44:27 +1000

    comment

---
+++ Easy/66. Plus one.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ *
+ * 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+ *
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+ *
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ * 示例1：
+ *
+ * 输入：digits = [1,2,3]
+ * 输出：[1,2,4]
+ * 解释：输入数组表示数字 123。
+ * 示例2：
+ *
+ * 输入：digits = [4,3,2,1]
+ * 输出：[4,3,2,2]
+ * 解释：输入数组表示数字 4321。
+ * 示例 3：
+ *
+ * 输入：digits = [0]
+ * 输出：[1]
+
+ */
+var plusOne = function(digits) {
+    let len = digits.length;
+    let i = len - 1;
+    while (i >= 0) {
+        if (digits[i] === 9) {
+            digits[i] = 0;
+            i--;
+        } else {
+            digits[i]++;
+            break;
+        }
+    }
+    if (i < 0) {
+        digits.unshift(1);
+    }
+    return digits;
+};
+
