7fb1382184b60c97cf73605c12c315f41e059858
Tianyu <tianyuma1508977@gmail.com>
2022-04-15 17:50:33 +1000

    comment

---
+++ Medium/46. Permutations.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ * Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,2,3]
+ * Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * Example 2:
+ *
+ * Input: nums = [0,1]
+ * Output: [[0,1],[1,0]]
+ * Example 3:
+ *
+ * Input: nums = [1]
+ * Output: [[1]]
+ */
+var permute = function(nums) {
+
+}
